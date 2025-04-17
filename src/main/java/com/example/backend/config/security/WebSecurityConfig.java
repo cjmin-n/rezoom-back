03e0e30305f68cc84e26d994c8316bc4cb29e595
cjmin_n <mymemose@naver.com>
2025-04-17 15:16:24 +0900

    fix: csrf 대응 추가

--- src/main/java/com/example/backend/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/backend/config/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -41,7 +42,17 @@ public class WebSecurityConfig {
 
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // 프론트가 읽을 수 있어야 하니까 false
+                        .ignoringRequestMatchers(
+                                "/auth/login",
+                                "/auth/signup",
+                                "/auth/token/logout",
+                                "/auth/token/refresh",
+                                "/auth/check-email"
+                        )
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/uploads/**","/webhook-front","/webhook-back","/files/**").permitAll()
