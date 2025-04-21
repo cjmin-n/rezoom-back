939843f8cc97ca96088c7e4378f70a0108fea0dc
sonyeoul <thswjd6@gmail.com>
2025-04-21 16:45:57 +0900

    ci: SSL,ALB

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOriginPatterns("http://localhost:3000")  // React 주소
+                .allowedOriginPatterns("http://localhost:3000","https://rezoom.netlify.app")  // React 주소
                 .allowedMethods("*")  // GET, POST 등 모두 허용
                 .allowedHeaders("*")    // Authorization 헤더 포함해서 모든 요청 헤더 허용
                 .allowCredentials(true);    // 쿠키, 인증 관련 헤더 포함 허용 (Authorization 포함
