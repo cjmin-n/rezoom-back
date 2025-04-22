ce8b157c72470ec5bf428c2bfc4a809c5c931807
sonyeoul <thswjd6@gmail.com>
2025-04-22 17:44:39 +0900

    feat:CORS add

---
+++ src/main/java/com/example/backend/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 URL 경로 허용
+                .allowedOrigins("https://rezoom.netlify.app") // 프론트엔드 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키, 인증 포함 시 true 필수
+    }
+}
